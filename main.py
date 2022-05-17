a78f20921af7ce79dfdfdaa677f1be7e3b5fea6c
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-05-17 10:12:25 +0200

    Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 df = pd.read_csv('Trojan_Asteroid_JPLQuery.csv')
 
@@ -6,9 +7,17 @@ main_data = df.to_dict()
 
 print(len(main_data['spkid']))
 
-e_list = []
+spkid_list = []
 
-for n in main_data['e'].items():
-    e_list.append(n)
+for n in main_data['spkid'].items():
+    spkid_list.append(n)
 
-print(e_list[20])
+print(spkid_list[2])
+
+# SBDB Request link: https://ssd-api.jpl.nasa.gov/sbdb.api
+
+spkid = spkid_list[2]
+
+req = requests.get(f'https://ssd-api.jpl.nasa.gov/sbdb.api?spk={spkid}&phys-par=1')
+
+print(req)
