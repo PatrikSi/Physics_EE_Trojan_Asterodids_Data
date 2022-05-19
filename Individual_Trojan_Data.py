7f2c66bb0818cf7e4d87cb8b9e65a971d4dfe650
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-05-19 12:31:12 +0200

    Commit

--- Individual_Trojan_Data.py
+++ Individual_Trojan_Data.py
@@ -7,13 +7,14 @@ x = []
 y = []
 z = []
 
-with open('2000624_empherides.txt', newline='') as empherides:
+with open('2001437.txt', newline='') as empherides:
     reader = csv.DictReader(empherides)
     for row in reader:
         calendar_date.append(row['            Calendar Date (TDB)'])
-        JDTDB.append(row['            JDTDB'])
+        JDTDB.append(row['          JDTDB'])
         x.append(row['                      X'])
         y.append(row['                      Y'])
         z.append(row['                      Z'])
-        X = [n.strip(' ') for n in x]
-        print(x)
+        xs = []
+    plt.scatter(x, y)
+    plt.show()
