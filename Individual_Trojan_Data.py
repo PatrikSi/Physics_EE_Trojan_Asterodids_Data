1bcbf7885546078be562fcf7935cacf56aa4b149
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-05-20 11:07:26 +0200

    Commit

--- Individual_Trojan_Data.py
+++ Individual_Trojan_Data.py
@@ -1,20 +1,41 @@
 import csv
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import numpy as np
+
 name_list = []
 JDTDB = []
 calendar_date = []
 x = []
 y = []
 z = []
+ax = plt.axes(projection="3d")
 
-with open('2001437.txt', newline='') as empherides:
+with open('horizons_results_4_x-y.txt', newline='') as empherides:
     reader = csv.DictReader(empherides)
     for row in reader:
         calendar_date.append(row['            Calendar Date (TDB)'])
-        JDTDB.append(row['          JDTDB'])
-        x.append(row['                      X'])
-        y.append(row['                      Y'])
-        z.append(row['                      Z'])
-        xs = []
-    plt.scatter(x, y)
-    plt.show()
+        JDTDB.append(row['            JDTDB'])
+        x.append(float(row['                      X']))
+        y.append(float(row['                      Y']))
+        z.append(float(row['                      Z']))
+    print(x)
+    print(len(x))
+    ax.scatter3D(x, y, z, color='red')
+
+    xj = []
+    yj = []
+    zj = []
+
+with open('Jupiter_position_data.txt', newline='') as empherides:
+    reader = csv.DictReader(empherides)
+    for row in reader:
+        calendar_date.append(row['            Calendar Date (TDB)'])
+        JDTDB.append(row['            JDTDB'])
+        xj.append(float(row['                      X']))
+        yj.append(float(row['                      Y']))
+        zj.append(float(row['                      Z']))
+
+    ax.scatter3D(xj, yj, zj, color='orange')
+plt.title('Scatter')
+plt.show()
