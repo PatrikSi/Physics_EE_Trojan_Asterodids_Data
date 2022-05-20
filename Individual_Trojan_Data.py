a2517055f2d76a58afbf3623d08e5296639ed9db
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-05-20 11:15:57 +0200

    Commit

--- Individual_Trojan_Data.py
+++ Individual_Trojan_Data.py
@@ -19,7 +19,10 @@ with open('horizons_results_4_x-y.txt', newline='') as empherides:
         x.append(float(row['                      X']))
         y.append(float(row['                      Y']))
         z.append(float(row['                      Z']))
+    print('Position data for Trojan Asteroid:')
     print(x)
+    print(y)
+    print(z)
     print(len(x))
     ax.scatter3D(x, y, z, color='red')
 
@@ -35,7 +38,31 @@ with open('Jupiter_position_data.txt', newline='') as empherides:
         xj.append(float(row['                      X']))
         yj.append(float(row['                      Y']))
         zj.append(float(row['                      Z']))
-
+    print('Position data for Jupiter:')
+    print(xj)
+    print(yj)
+    print(zj)
     ax.scatter3D(xj, yj, zj, color='orange')
+
+xm = []
+ym = []
+zm = []
+
+with open('Mars_position_data.txt', newline='') as empherides:
+    reader = csv.DictReader(empherides)
+    for row in reader:
+        calendar_date.append(row['            Calendar Date (TDB)'])
+        JDTDB.append(row['            JDTDB'])
+        xm.append(float(row['                      X']))
+        ym.append(float(row['                      Y']))
+        zm.append(float(row['                      Z']))
+    print('Position data for Mars:')
+    print(xm)
+    print(ym)
+    print(zm)
+    ax.scatter3D(xm, ym, zm, color='purple', label='Mars')
+
+
+
 plt.title('Scatter')
 plt.show()
