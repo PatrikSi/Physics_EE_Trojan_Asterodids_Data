a7fbac142a1d94dbf2cd8e6227d56c7d96615ec4
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-05-20 16:45:57 +0200

    Commit

--- Individual_Trojan_Data.py
+++ Individual_Trojan_Data.py
@@ -2,6 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import numpy as np
+import pandas as pd
 
 name_list = []
 JDTDB = []
@@ -35,6 +36,8 @@ ax = plt.axes(projection="3d")
 xj = []
 yj = []
 zj = []
+t = 10 # Time to show all the objects
+# JDTDB time to de determined by JDTDB = 2378496.5 + 60t
 
 with open('Planetary_Orbital_Data/Jupiter_position_data.txt', newline='') as empherides:
     reader = csv.DictReader(empherides)
@@ -48,7 +51,8 @@ with open('Planetary_Orbital_Data/Jupiter_position_data.txt', newline='') as emp
     print(xj)
     print(yj)
     print(zj)
-    ax.scatter3D(xj, yj, zj, color='orange', label='Jupiter', s=1)
+    ax.scatter3D(xj[t], yj[t], zj[t], color='orange', label='Jupiter', s=25)
+    ax.scatter3D(xj, yj, zj, color='orange', label='Jupiter Orbit', s=0.25)
 
 xm = []
 ym = []
@@ -66,7 +70,9 @@ with open('Planetary_Orbital_Data/Mars_position_data.txt', newline='') as empher
     print(xm)
     print(ym)
     print(zm)
-    ax.scatter3D(xm, ym, zm, color='purple', label='Mars', s=1)
+    ax.scatter3D(xm[t], ym[t], zm[t], color='purple', label='Mars', s=25)
+    ax.scatter3D(xm, ym, zm, color='purple', label='Mars Orbit', s=0.25)
+
 
 xs = []
 ys = []
@@ -84,11 +90,13 @@ with open('Planetary_Orbital_Data/Saturn_position_data.txt', newline='') as emph
     print(xs)
     print(ys)
     print(zs)
-    ax.scatter3D(xs, ys, zs, color='blue', label='Saturn', s=1)
+    ax.scatter3D(xs[t], ys[t], zs[t], color='blue', label='Saturn', s=25)
+    ax.scatter3D(xs, ys, zs, color='blue', label='Saturn Orbit', s=0.25)
 
 xe = []
 ye = []
 ze = []
+Calendar_Date = []
 
 
 with open('Planetary_Orbital_Data/Earth_position_data.txt', newline='') as empherides:
@@ -99,15 +107,27 @@ with open('Planetary_Orbital_Data/Earth_position_data.txt', newline='') as emphe
         xe.append(float(row['                      X']))
         ye.append(float(row['                      Y']))
         ze.append(float(row['                      Z']))
+        calendar_date.append(row['            Calendar Date (TDB)'])
+
     print('Position data for Earth:')
     print(xe)
     print(ye)
     print(ze)
-    ax.scatter3D(xe, ye, ze, color='yellow', label='Earth', s=1)
+    print(f'Showing Positions for time {calendar_date[t]}')
+    ax.scatter3D(xe[t], ye[t], ze[t], color='yellow', label='Earth', s=25)
+    ax.scatter3D(xe, ye, ze, color='yellow', label='Earth Orbut', s=0.25)
 
 
 # ------------------------------------------------Collecting SPKID from Horizons Index---------------------------------
 
-plt.title('Orbital Data of the Solar System')
+df = pd.read_csv('Trojan_Asteroid_JPLQuery.csv')
+main_data = df.to_dict()
+
+spkid_list = []
+
+for n in main_data['spkid'].items():
+    spkid_list.append(n)
+
+plt.title(f'Orbital Data of the Solar System at time {calendar_date[t][:18]}')
 plt.legend()
 plt.show()
