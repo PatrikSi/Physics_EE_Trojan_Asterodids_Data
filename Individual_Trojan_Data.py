3ce2aa5a3694b3d4cf39baf5b0a5b11b8fd769a5
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-05-23 11:46:09 +0200

    Commit

--- Individual_Trojan_Data.py
+++ Individual_Trojan_Data.py
@@ -216,7 +216,7 @@ for spkid in trojan_list:
         print(y)
         print(z)
         print(len(x))
-        ax.scatter3D(x, y, z, color='grey', s=0.001)
+        # ax.scatter3D(x, y, z, color='grey', s=0.001)
         ax.scatter3D(x[t], y[t], z[t], color='red', s=3)
 
 # ---------------------------------------------plotting-----------------------------------------------------------------
