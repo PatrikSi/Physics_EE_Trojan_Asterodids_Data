447b05c9e2cc37306ea8822a1579f5b9256ee469
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-07-23 11:07:15 +0200

    commit

--- Two_body_diagram.py
+++ Two_body_diagram.py
@@ -1,13 +1,21 @@
 import matplotlib.pyplot as plt
 
+sx = -0.6
+sy = 0
+
+ex = 1.5
+ey = 0
+
 figure, ax = plt.subplots()
 plt.xlim(-1.5, 3)
 plt.ylim(-2, 2)
 
 
-sun = plt.Circle((-0.4, 0), 0.3, fill=False, label='Body 1')
-earth = plt.Circle((1.5, 0), 0.1, fill=False, label='Body 2')
+sun = plt.Circle((sx, sy), 0.3, fill=False, label='Body 1')
+earth = plt.Circle((ex, ey), 0.1, fill=False, label='Body 2')
 
+v_1 = plt.plot([0,sx], [0,sy], linestyle='-', label='v1', color='black')
+v_2 = plt.plot([0,ex], [0,ey], linestyle='-', label='v2', color='black')
 
 plt.plot([0], marker='x', label='Barycenter', color='black')
 
