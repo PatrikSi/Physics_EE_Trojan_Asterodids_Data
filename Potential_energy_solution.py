6630acfee895c3a64f6e8d370373e9fc1e029902
Patrik Sivak <patrik.sivak.314@gmail.com>
2022-08-20 15:09:59 +0200

    commit

--- Potential_energy_solution.py
+++ Potential_energy_solution.py
@@ -5,13 +5,13 @@ from matplotlib import cm
 M_s = 1988500*10**24
 M_j = 1898*10**24
 
-sc = 0
+sc = 1.5
 mu = (M_j/M_s)*10**sc
 R = 1
 sun_pos = np.array([-mu*R, 0])
 earth_pos = np.array([(1-mu)*R, 0])
 
-N = 2000
+N = 500
 x, y = np.meshgrid(np.linspace(-1.5, 1.5, N), np.linspace(-1.5, 1.5, N))
 
 term1 = -(1-mu) / ((x + mu)**2 + y**2)**0.5
@@ -21,7 +21,7 @@ term3 = -0.5 * (x**2 + y**2)
 u = term1 + term2 + term3
 
 lw = 0.1
-level = 50000
+level = 200
 plt.figure(figsize=(5, 5))
 plt.ylim(-1.5, 1.5)
 levels = np.linspace(-4, -1, level)
